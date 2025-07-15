b104ab2ef0171159fb9484e6e05d05fc63115d30
MARCOSAWADA <marcos_sawada@hotmail.com>
2025-07-15 11:03:48 -0400

    navbar pronto

---
+++ Public/includes/navbar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Navbar Buy at Home</title>
+    <link rel="stylesheet" href="../../Public/css/main.css">
+</head>
+<body class="navbar">
+
+<nav>
+  <div class="logo-container">
+    <img src="../../Public/img/logo.avif" alt="Logo Buy at Home" />
+    <p>Buy</p>
+    <span>At</span>
+    <p>Home</p>
+  </div>
+
+  <div class="menu" id="menu">
+    <a href="#">Home</a>
+    <a href="#">Produtos</a>
+    <a href="#">Contato</a>
+    <a href="#">Sobre</a>
+  </div>
+
+  <div class="hamburger" id="hamburger">
+    <img src="../../Public/img/Frame.png" alt="Menu"/>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+
+</nav>
+
+<script src="../../Public/js/navbar.js"></script>
+</body>
+</html>

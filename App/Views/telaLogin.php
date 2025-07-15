e5b3a6c7c73aeca953e73edc12960f0b9dc54886
Vitor-NLF <loureirovitornascimento@gmail.com>
2025-07-15 10:10:31 -0400

    subindo a tela de login. Html e Css

---
+++ App/Views/telaLogin.php
@@ -0,0 +1,31 @@
+<?php
+include '../../Public/includes/header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="bodyLogin">
+    <section class="areaLogin">
+        <h1><img src="../../Public/img/logo_mg.png" alt="">Buy<span>At</span>Home</h1>
+        <h2>Login</h2>
+        <p>Entre em sua conta para ter
+        acesso a conteudos incríveis!</p>
+
+        <form action="" class="formLogin">
+            <div class="areaInput">
+                <i class="fa-solid fa-at"></i><input type="email" name="" placeholder="email">
+            </div>
+            <div class="areaInput">
+                <i class="fa-solid fa-lock"></i><input type="password" name="" placeholder="senha">
+            </div>
+           
+           
+            <input type="submit" value="Fazer Login">
+        </form>
+    </section>
+</body>
+</html>
--- Public/includes/header.php
+++ Public/includes/header.php
@@ -4,4 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Public/css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Tourney:ital,wght@0,900;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
 </head>

4782d8f623a359c73602e026e1e860dbbb61989a
João Almeida <joao54169816@aluno.ms.senac.br>
2025-07-15 10:12:24 -0400

    feat: criado tela inicial com opções de cadastrar ou logar.

---
+++ App/Views/telaInicial.php
@@ -0,0 +1,32 @@
+<?php include '../../Public/includes/header.php'; ?>
+
+<title>Tela Inicial</title>
+
+<main>
+    <div class="telaInicial-wrapper">
+
+        <div class="telaInicial-logo-wrapper">
+
+            <div class="telaInicial-logo">
+                
+                <img src="../../Public/img/logo.avif" alt="">
+                <p>Buy <span>at</span> Home</p>
+
+            </div>
+            
+            <div class="telaInicial-description">
+                <p>Bem Vindo!</p>
+            </div>
+
+        </div>
+
+
+        <div class="telaInicial-buttons">
+
+            <button>Cadastrar-se</button>
+            <button>Logar</button>
+        
+        </div>
+
+    </div>
+</main>

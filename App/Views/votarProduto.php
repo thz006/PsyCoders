632e649ca45633ec40f9e26460fe9eca41e668f7
João Almeida <toxicyszconta@gmail.com>
2025-07-16 01:23:37 -0300

    feat: front da tela de votar em produtos feita, falta criar a tabelinha de usuarios que mais votaram

---
+++ App/Views/votarProduto.php
@@ -0,0 +1,117 @@
+<?php
+include '../../Public/includes/header.php';
+?>
+<link rel="stylesheet" href="../../Public/css/swiper-bundle.css">
+<script defer src="../../Public/js/swiper-bundle.js"></script>
+<script defer src="../../Public/js/votarProduto.js"></script>
+<title>Votar Produtos</title>
+
+<body>
+	<div class="votarProduto-Sidebar">
+
+		<div class="votarProduto-logo">
+			<img src="../../Public/img/logo.avif" alt="">
+			<h1>Buy <span>At</span> Home</h1>
+		</div>
+
+		<div class="votarProduto-burguer">
+			<i class="fa-solid fa-bars"></i>
+		</div>
+
+	</div>
+
+	<section class="votarProduto-topSemanal">
+
+		<div class="votarProduto-topText">
+			<h1>Top da Semana</h1>
+			<p>Vote e ganhe desconto!</p>
+		</div>
+
+		<div class="votarProduto-carousel">
+						
+			<!-- Slider main container -->
+			<div class="swiper" id="swiper-inicial">
+			  <!-- Additional required wrapper -->
+			  <div class="swiper-wrapper">
+			    <!-- Slides -->
+			    <div class="swiper-slide">
+
+			    	<div class="votarProduto-Produto">
+
+			    		<img src="../../Public/img/kroCebola.webp" alt="">
+
+			    		<div class="votarProduto-prodInfo">
+
+			    			<div class="votarProduto-prodText">
+			    				<h1>Kró Cebola</h1>
+			    				<p><span>268</span> votos</p>
+			    			</div>
+
+							<i class="fa-solid fa-thumbs-up"></i>
+
+			    		</div>
+
+			    	</div>
+
+			    </div>
+
+			    <div class="swiper-slide">
+
+			    	<div class="votarProduto-Produto">
+
+			    		<img src="../../Public/img/kroCebola.webp" alt="">
+
+			    		<div class="votarProduto-prodInfo">
+
+			    			<div class="votarProduto-prodText">
+			    				<h1>Kró Cebola</h1>
+			    				<p><span>268</span> votos</p>
+			    			</div>
+
+							<i class="fa-solid fa-thumbs-up"></i>
+
+			    		</div>
+
+			    	</div>
+
+			    </div>
+
+			    <div class="swiper-slide">
+
+			    	<div class="votarProduto-Produto">
+
+			    		<img src="../../Public/img/kroCebola.webp" alt="">
+
+			    		<div class="votarProduto-prodInfo">
+
+			    			<div class="votarProduto-prodText">
+			    				<h1>Kró Cebola</h1>
+			    				<p><span>268</span> votos</p>
+			    			</div>
+
+							<i class="fa-solid fa-thumbs-up"></i>
+
+			    		</div>
+
+			    	</div>
+
+			    </div>
+
+
+
+			  </div>
+
+			  <!-- If we need pagination -->
+			  <div class="swiper-pagination"></div>
+
+			  If we need navigation buttons
+<!-- 			  <div class="swiper-button-prev"></div>
+			  <div class="swiper-button-next"></div> -->
+
+			</div>
+
+		</div>
+
+	</section>
+
+</body>
---
+++ Public/js/votarProduto.js
@@ -0,0 +1,14 @@
+new Swiper('#swiper-inicial', {
+  slidesPerView: 'auto',     // deixa o CSS decidir a largura
+  centeredSlides: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  // loop: true,                // garante um vizinho à esquerda desde o 1º frame
+  initialSlide: 1,           // começa já com um cartão de cada lado
+  pagination: { el: '.swiper-pagination', clickable: true },
+
+  // No desktop você volta para três inteiros
+  breakpoints: {
+    768: { slidesPerView: 3, spaceBetween: 24 }
+  }
+});
